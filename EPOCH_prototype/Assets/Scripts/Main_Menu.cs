527e36bd935babb13f3b04e8805b71829073f77b
KageHayashi <kagehayashi@gmail.com>
2019-12-01 14:42:43 -0800

    Added title card and menus

---
+++ EPOCH_prototype/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour {
+    public void playGame() {
+        SceneManager.LoadScene("Training");
+
+        //loads the next scene in queue
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Application.Quit();   
+    }
+}
---
+++ EPOCH_prototype/Assets/Scripts/Title_Screen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_Screen : MonoBehaviour {
+
+    public void Update() {
+        if (Input.anyKeyDown) {
+            Start_Game();
+        }
+    }
+
+    public void Start_Game() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
