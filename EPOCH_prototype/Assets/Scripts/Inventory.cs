eaa32c659392037188e6fd3cee4b23079343a6ea
KageHayashi <kagehayashi@gmail.com>
2019-12-03 00:48:19 -0800

    added basic pause menu functionalities

---
+++ EPOCH_prototype/Assets/Scripts/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private bool InvEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slots;
+
+    public GameObject slotHolder;
+
+    void Start() {
+        allSlots = 6;
+        slots = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++) {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            InvEnabled = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Tab)) {
+            InvEnabled = false;
+        }
+
+        if (InvEnabled) {
+            inventory.SetActive(true);
+        }
+        else {
+            inventory.SetActive(false);
+        }
+    }
+}
---
+++ EPOCH_prototype/Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour {
+
+    public static bool GamePause = false;
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GamePause) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GamePause = false;
+    }
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+    public void loadMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void quitGame() {
+
+    }
+}
